23c31dee32bb28103c4ba7bc5b53ca4fb5b4cfa9
Craig Thomas <craig.thomas@bhnetwork.com>
2015-07-07 07:37:58 -0700

    checkpoint with functional dynamodb implementation.

--- src/digester.js
+++ src/digester.js
@@ -2,6 +2,7 @@ console.log('Loading function');
 var aws = require('aws-sdk');
 var s3 = new aws.S3();
 var sns = new aws.SNS();
+var dynamodb = new aws.DynamoDB();
 
 exports.handler = function(event, context) {
     console.log("Received event ", JSON.stringify(event, null, 2));
@@ -9,120 +10,121 @@ exports.handler = function(event, context) {
     var bucket = event.Records[0].s3.bucket.name;
     var key = event.Records[0].s3.object.key;
 
-    console.log("get md5 of event's object.");
-
-    if (key.substring(0, 9) != 'accepted-') {
-
-        // Do a HEAD request on the object from the event and get its ETag (MD5)
-        s3.headObject({Bucket: bucket, Key: key}, function(err, headData) {
-            console.log("checking for duplicates.");
-            if (err) {
-                console.log("Error getting object " + key + " from bucket " + bucket +
-                ". Make sure they exist and the bucket is in the same region as this function.");
-                context.fail("Error getting file: " + err);
-            } else {
-                var eTag = headData.ETag;
-                console.log("New file's eTag: ", eTag);
-                // Look at all the other objects in the S3 bucket
-                s3.listObjects({Bucket: bucket}, function(err, listData) {
-                    console.log(listData.Contents);
-                    var match;
-                    for (var file in listData.Contents) {
-                        console.log("Existing file: ", listData.Contents[file].Key, "eTag: ", listData.Contents[file].ETag);
-                        // Check if the file checksums match
-                        if ((eTag == listData.Contents[file].ETag ) && (key != listData.Contents[file].Key)) {
-                            match = file;
-                            console.log("there was a match");
-                            break;
+    // Do a HEAD request on the object from the event and get its ETag (MD5)
+    s3.headObject({Bucket: bucket, Key: key}, function(err, headData) {
+        if (err) {
+            console.log("Error getting object " + key + " from bucket " + bucket +
+            ". Make sure they exist and the bucket is in the same region as this function.");
+            context.fail("Error getting file: " + err);
+        } else {
+            var eTag = headData.ETag;
+            console.log("New file's eTag: ", eTag);
+            // Insert into the dynamodb table, documents-received, using the etag as the 
+            // primary key. If the insert fails, a file with an identical hash has already
+            // been processed, so reject the duplicate. If the insert succeeds, then the
+            // etag is recorded so that subsequent duplicates will be detected.
+            var tableName = "documents-received";
+            var receivedDateTime = new Date().toJSON();
+            var receivedItem = {
+                "etag": {"S": eTag},
+                "receivedDateTime": {"S": receivedDateTime},
+                "fileName": {"S": key}
+            };
+            dynamodb.putItem({
+                "TableName": tableName,
+                "ConditionExpression":  "etag <> :e",
+                "ExpressionAttributeValues": {
+                    ":e": {"S": eTag}
+                },
+                "Item": receivedItem
+            }, function(err, data) {
+                if (err) {
+                    console.log('putting item into dynamodb failed: '+err);
+                    // If there was a duplicate, then (1) retrieve the matched data from
+                    // dynamodb; (2) publish to SNS; (3) copy the file to the inerror bucket;
+                    // (4) delete the file from the indigestion bucket.
+                    dynamodb.getItem({
+                        "TableName": tableName,
+                        "Key": {
+                            "etag": {"S": eTag}
+                        },
+                        "ProjectionExpression": "etag, receivedDateTime, fileName"
+                    }, function(err, data) {
+                        if (err) {
+                            console.log("error reading data", err);
+                        } else {
+                            console.log("got item data", JSON.stringify(data, null, 2));
+                            var existingFileName = data.Item.fileName.S;
+                            console.log("existingFileName: ", existingFileName);
+                            var payload = "Uploaded file " + key + " matches existing file " + existingFileName + "\n" + JSON.stringify(data, null, 2);
+                            var snsParams = {
+                                Message: payload,
+                                Subject: 'Duplicate file received',
+                                TopicArn: 'arn:aws:sns:us-west-2:134529390395:digester-alarm'
+                            };
+                            console.log(snsParams);
+                            sns.publish(snsParams, function(err, data) {
+                                if (err) {
+                                    console.log("Error sending SNS ", err);
+                                } else {
+                                    console.log("published to sns.");
+                                    var copyParams = {
+                                        Bucket: 'inerror',
+                                        CopySource: 'indigestion' + '/' + key,
+                                        Key: key
+                                    };
+                                    s3.copyObject(copyParams, function(err, data) {
+                                        if (err) console.log(err, err.stack); // an error occurred
+                                        else {
+                                            console.log("Copied duplicate file to inerror");
+                                            // now delete the file from indigestion
+                                            var deleteParams = {
+                                                Bucket: bucket,
+                                                Key: key
+                                            };
+                                            console.log(deleteParams);
+                                            s3.deleteObject(deleteParams, function(err, data) {
+                                                if (err) console.log(err, err.stack); // an error occurred
+                                                else {
+                                                    console.log("Deleted file from indigestion");           // successful response
+                                                    context.succeed();
+                                                }
+                                            });
+                                        }
+                                    });
+                                }
+                            });
                         }
-                    }
-                    // If there was a duplicate, then publish to SNS
-                    if (match) {
-                        console.log("handle the match");
-                        var payload = "Uploaded file " + key + " matches existing file " + listData.Contents[file].Key;
-                        var snsParams = {
-                            Message: payload,
-                            Subject: 'Duplicate file received',
-                            TopicArn: 'arn:aws:sns:us-west-2:134529390395:digester-alarm'
-                        };
-                        console.log(snsParams);
-                        sns.publish(snsParams, function(err, data) {
-                            if (err) {
-                                console.log("Error sending SNS ", err);
-                            } else {
-                                console.log("published to sns.");
-                                console.log("Duplicate file received: " + listData.Contents[file].Key);
-                                var copyParams = {
-                                    Bucket: 'inerror',
-                                    CopySource: 'indigestion' + '/' + key,
-                                    Key: key
-                                };
-                                console.log(copyParams);
-                                s3.copyObject(copyParams, function(err, data) {
-                                    if (err) console.log(err, err.stack); // an error occurred
-                                    else {
-                                        console.log(data);           // successful response
-                                        // now delete the file from indigestion
-                                        var deleteParams = {
-                                            Bucket: bucket,
-                                            Key: key
-                                        };
-                                        console.log(deleteParams);
-                                        s3.deleteObject(deleteParams, function(err, data) {
-                                            if (err) console.log(err, err.stack); // an error occurred
-                                            else {
-                                                console.log(data);           // successful response
-                                                context.succeed();
-                                            }
-                                        });
-                                    }
-                                });
-                            }
-                        });
-                    } else {
-                        // otherwise, copy the file to inprogress bucket then rename it in indigestion
-                        console.log("new file, so copy.");
-                        var copyParams = {
-                            Bucket: 'inprogress',
-                            CopySource: 'indigestion' + '/' + key,
-                            Key: key
-                        };
-                        console.log(copyParams);
-                        s3.copyObject(copyParams, function(err, data) {
-                            if (err) console.log(err, err.stack); // an error occurred
-                            else {
-                                console.log(data);           // successful response
-                                copyParams = {
-                                    Bucket: 'indigestion',
-                                    CopySource:  'indigestion' + '/' + key,
-                                    Key: 'accepted-' + key
-                                };
-                                s3.copyObject(copyParams, function(err, data) {
-                                    if (err) console.log(err, err.stack); // an error occurred
-                                    else {
-                                        console.log(data);
-                                        // now delete the file from indigestion
-                                        var deleteParams = {
-                                            Bucket: bucket,
-                                            Key: key
-                                        };
-                                        console.log(deleteParams);
-                                        s3.deleteObject(deleteParams, function(err, data) {
-                                            if (err) console.log(err, err.stack); // an error occurred
-                                            else {
-                                                console.log(data);           // successful response
-                                                context.succeed();
-                                            }
-                                        });
-                                    }
-                                });
-                            }
-                        });
-                    }
-                });
-            }
-        });
-    } else {
-        context.succeed();
-    }
+                    });
+                } else {
+                    // otherwise, copy the file to inprogress bucket then delete it from indigestion
+                    console.log("new file, so copy to inprogress");
+                    var copyParams = {
+                        Bucket: 'inprogress',
+                        CopySource: 'indigestion' + '/' + key,
+                        Key: key
+                    };
+                    s3.copyObject(copyParams, function(err, data) {
+                        if (err) {
+                            console.log(err, err.stack); // an error occurred
+                        } else {
+                            console.log("copied to inprogress");  // successful response
+                            // now delete the file from indigestion
+                            var deleteParams = {
+                                Bucket: bucket,
+                                Key: key
+                            };
+                            s3.deleteObject(deleteParams, function(err, data) {
+                                if (err) console.log(err, err.stack); // an error occurred
+                                else {
+                                    console.log("deleted file from indigestion");           // successful response
+                                    context.succeed();
+                                }
+                            });
+                        }
+                    });
+                }
+            });
+        }
+    });
 };
